da5538c77c02530deff3d9311513239d9a5e2270
mohammad reza <ebadi4760@gmail.com>
2024-03-12 13:14:27 +0330

    first commit

---
+++ lib/bloc/basket/basket_bloc.dart
@@ -0,0 +1,26 @@
+import 'package:apple_shop/bloc/basket/baset_event.dart';
+import 'package:apple_shop/bloc/basket/basket_state.dart';
+import 'package:apple_shop/util/pyament_handler.dart';
+import 'package:bloc/bloc.dart';
+
+import '../../data/repository/basket_repository.dart';
+
+class BasketBloc extends Bloc<BasketEvent, BasketState> {
+  final IBasketRepository _basketRepository;
+  final PaymentHandler _paymentHandler;
+  BasketBloc(this._paymentHandler,this._basketRepository) : super(BasketInitState()) {
+    on<BasketFetchFromHiveEvent>(((event, emit) async {
+      var basketItemList = await _basketRepository.getAllBasketItems();
+      var finalPrice = await _basketRepository.getBasketFinalPrice();
+      emit(BasketDataFetchedState(basketItemList, finalPrice));
+    }));
+
+    on<BasketPaymentInitEvent>((event, emmit) async {
+      _paymentHandler.initPaymentRequest();
+    });
+
+    on<BasketPaymentRequestEvent>((event, emmit) async {
+      _paymentHandler.sendPaymentRequest();
+    });
+  }
+}
